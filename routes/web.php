7feb1b0e0d4a0d8739cd84e9cdc3ad32a0fcfc37
Marvellous010 <9025915@student.zadkine.nl>
2025-12-02 12:55:51 +0100

    Opdracht week 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,3 +14,22 @@ Route::get('/contact', function () {
 Route::get('/layout', function () {
     return view('layout-page');
 })->name('layout.page');
+
+
+Route::get('/user/add', function () {
+    User::create([
+        'name' => 'Nieuwe User',
+        'email' => 'user'.rand(1,9999).'@example.com',
+        'password' => bcrypt('password')
+    ]);
+
+    return 'Nieuwe user toegevoegd!';
+});
+
+Route::get('/users', function () {
+    $users = User::all();
+
+    foreach ($users as $user) {
+        echo $user->id . ' - ' . $user->name . ' - ' . $user->email . '<br>';
+    }
+});
